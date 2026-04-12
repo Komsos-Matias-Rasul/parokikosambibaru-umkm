41ae8031bbac9e59b62ad8201ef1614975497bbc
PetrusAriaa <petrusariacr25@gmail.com>
2026-04-12 14:02:27 +0700

    feat: add promotion banner contents

--- src/components/homepage/PromotionBanner.jsx
+++ src/components/homepage/PromotionBanner.jsx
@@ -3,9 +3,9 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, Pagination } from 'swiper/modules';
 
-// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
+import Image from 'next/image';
 
 export const PromotionBanner = () => {
   return (
@@ -14,13 +14,13 @@ export const PromotionBanner = () => {
         modules={[Autoplay, Pagination]}
         pagination={{ clickable: true }}
         autoplay={{ delay: 3000 }}
-        className="h-75 md:h-100 bg-gray-200"
+        className="min-h-20vh aspect-16/5 bg-slate-200"
       >
-        <SwiperSlide className="flex items-center justify-center bg-blue-50 text-2xl font-bold opacity-50">
-          Banner Promotion 1
+        <SwiperSlide className="flex items-center justify-center">
+          <Image src="/umkm-banner-1.jpg" width={1600} height={500} alt=""  />
         </SwiperSlide>
-        <SwiperSlide className="flex items-center justify-center bg-blue-100 text-2xl font-bold opacity-50">
-          Banner Promotion 2
+        <SwiperSlide className="flex items-center justify-center">
+          <Image src="/umkm-banner-2.jpg" width={1600} height={500} alt=""  />
         </SwiperSlide>
       </Swiper>
     </section>
